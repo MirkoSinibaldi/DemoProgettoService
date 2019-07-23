0cd108ab5ceb8d6c77a57532c21171a62b2db0d8
Mirko Sinibaldi <sinibaldimirk88@gmail.com>
2019-07-23 16:26:14 +0200

    Implementazione DTO Publisher e Genre

--- src/main/java/it/k2/demo/demoProgettoService/models/GenreDto.java
+++ src/main/java/it/k2/demo/demoProgettoService/models/GenreDto.java
@@ -1,4 +1,20 @@
 package it.k2.demo.demoProgettoService.models;
 
-public class GenreDto {
+public class GenreDto
+{
+    private String description;
+
+    public GenreDto() {}
+
+    public GenreDto(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
