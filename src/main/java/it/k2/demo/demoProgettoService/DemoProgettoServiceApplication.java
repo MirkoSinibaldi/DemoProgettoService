cc51be2424000da23878a00051854a9a62b6dd0c
Mirko Sinibaldi <sinibaldimirk88@gmail.com>
2019-07-23 15:31:32 +0200

    Nuovo Progetto con i servizi Rest

---
+++ src/main/java/it/k2/demo/demoProgettoService/DemoProgettoServiceApplication.java
@@ -0,0 +1,13 @@
+package it.k2.demo.demoProgettoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProgettoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProgettoServiceApplication.class, args);
+	}
+
+}
